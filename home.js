d7905d3e865d0a18563eece3d7bd7db05a242dec
Abdulla_AL_Mamun <ahmedmamun2526@gmail.com>
2024-11-17 20:11:23 +0600

    transaction section

--- home.js
+++ home.js
@@ -6,13 +6,24 @@ document.getElementById('main-cashout-btn').addEventListener('click',function(){
     // console.log('main cash out clicked');
     document.getElementById('add-money-section').classList.add('hidden');
     document.getElementById('cash-out-section').classList.remove('hidden');
+    document.getElementById('transaction-section').classList.add('hidden');
 })
 //main-addmoney-btn
 document.getElementById('main-addmoney-btn').addEventListener('click',function(){
     // console.log('main cash out clicked');
     document.getElementById('add-money-section').classList.remove('hidden');
+    document.getElementById('transaction-section').classList.add('hidden');
     document.getElementById('cash-out-section').classList.add('hidden');
 })
+
+// main-Transaction-button
+document.getElementById('main-transaction-btn').addEventListener('click',function(){
+    document.getElementById('add-money-section').classList.add('hidden');
+    document.getElementById('cash-out-section').classList.add('hidden');
+    document.getElementById('transaction-section').classList.remove('hidden');
+})
+
+
 
 //Add money process
 document.getElementById('addMoney-btn').addEventListener('click',function(event){
@@ -24,6 +35,13 @@ document.getElementById('addMoney-btn').addEventListener('click',function(event)
     if(pin === '1234'){
         const balance= parseInt(mainAmmount) +parseInt(ammount) ;
      document.getElementById('main-ammount').innerText= balance;
+//transaction section
+    const transactionSection= document.getElementById('transaction-section');
+    const p= document.createElement('p');
+    p.innerText=`Added Money: ${parseInt(ammount)} Total Ammount:${parseInt(balance)}`;
+    transactionSection.appendChild(p);
+
+
     }
     else{
         alert('wrong password');
@@ -44,6 +62,13 @@ document.getElementById('cashout-btn').addEventListener('click',function(event){
     if(pin === '1234'){
         const balance= parseInt(mainAmmount) -parseInt(ammount) ;
      document.getElementById('main-ammount').innerText= balance;
+//transaction section
+const transactionSection= document.getElementById('transaction-section');
+const p= document.createElement('p');
+p.innerText=`Cashout money: ${parseInt(ammount)} Total Ammount:${parseInt(balance)}`;
+transactionSection.appendChild(p);
+
+
     }
     else{
         alert('wrong password');
